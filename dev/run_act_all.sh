2ded5832f265afc125ef73bf774e60080123c27d
Crow, Michael C <crowmc@ornl.gov>
2021-10-26 13:00:09 -0400

    added currently active, but now obsolete run script

---
+++ dev/run_act_all.sh
@@ -0,0 +1,18 @@
+cd /apps/adc/act/quicklooks/dailyquicklooks/
+source /opt/anaconda/anaconda3/etc/profile.d/conda.sh
+conda activate plotting_env
+
+# TODO: Move the txt files out of the main directory after executing act_plotting_main.py to clean it up a bit
+
+#ls /data/archive/sgp/ | grep -vE '.a1|.a0|.00' > sgp.txt
+#ls /data/archive/nsa/ | grep -vE '.a1|.a0|.00' > nsa.txt
+#ls /data/archive/ena/ | grep -vE '.a1|.a0|.00' > ena.txt
+#ls /data/archive/mos/ | grep -vE '.a1|.a0|.00' > mos.txt
+#ls /data/archive/cor/ | grep -vE '.a1|.a0|.00' > cor.txt
+#ls /data/archive/anx/ | grep -vE '.a1|.a0|.00' > anx.txt
+#ls /data/archive/oli/ | grep -vE '.a1|.a0|.00' > oli.txt
+ls /data/archive/guc/ | grep -vE '.00' > guc.txt
+#ls /data/archive/hou/ | grep -vE '.00' > hou.txt
+
+python act_plotting_main.py -nt 20 -sites hou,mos,cor,anx,oli,guc,nsa,ena,sgp -useTxtFile -days 120 -baseOut /var/ftp/quicklooks/ -p -ts -maxFs 200000000 >> logs/act.$(date +\%Y\%m\%d).log 2>&1
+
